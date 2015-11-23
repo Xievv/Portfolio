10fbacacc04c139d93e4cb5b415d90bce7ec7959
xievv <wolf00118@gmail.com>
2015-11-23 12:06:03 -0500

    Added ability to pull raw data in consoleapplication1

--- MyProjects/ConsoleApplication1/ConsoleApplication1/Program.cs
+++ MyProjects/ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -1,6 +1,4 @@
 ﻿using System;
-using System.Net;
-using System.IO;
 using System.Diagnostics;
 using System.Collections.Generic;
 using System.Linq;
@@ -14,23 +12,10 @@ namespace ConsoleApplication1
         static void Main(string[] args)
         {
             time();
-            htmlScrape();
+            pullWeather pull = new pullWeather();
+            string raw_data = pull.weatherScrape();
+            pull.weatherSearch(raw_data);
             Console.ReadKey();
-        }
-        static void htmlScrape()
-        {
-            string path = @"C:\Users\Shawn\Documents\Visual Studio 2015\Projects\html.txt";
-            string url = "view-source:https://www.google.com/search?q=google+weather&rlz=1C1CHFX_enUS653US653&oq=google+weather&aqs=chrome..69i57j0j69i60l2j0l2.1353j1j7&sourceid=chrome&es_sm=122&ie=UTF-8";
-            WebClient client = new WebClient();
-            string s = client.DownloadString(url);
-            try
-            {
-                File.WriteAllText(path, s);
-            }
-            catch(Exception e)
-            {
-                Console.WriteLine("Error: " + e);
-            }
         }
         static void time()
         {
---
+++ MyProjects/ConsoleApplication1/ConsoleApplication1/pullWeather.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Net;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class pullWeather
+    {
+        public string location;
+        public string skies;
+        public int temperature;
+
+        public string weatherScrape()
+        {
+            string data = null;
+            string urlAddress = "http://www.wunderground.com/cgi-bin/findweather/getForecast?query=Manchester%2C+NH";
+
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(urlAddress);
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+
+            if (response.StatusCode == HttpStatusCode.OK)
+            {
+                Stream receiveStream = response.GetResponseStream();
+                StreamReader readStream = null;
+
+                if (response.CharacterSet == null)
+                {
+                    readStream = new StreamReader(receiveStream);
+                }
+                else
+                {
+                    readStream = new StreamReader(receiveStream, Encoding.GetEncoding(response.CharacterSet));
+                }
+
+                data = readStream.ReadToEnd();
+
+                response.Close();
+                readStream.Close();
+
+            }
+            return data;
+        }
+        public void weatherSearch(string raw_data)
+        {
+            Console.WriteLine(raw_data);
+        }
+    }
+}
