7644ca4b27f3660fedc9f87962f77d35d5a47fff
xievv <wolf00118@gmail.com>
2015-11-28 15:01:21 -0500

    Added functionality to recipe jump tree using the displayText utility

--- schoolWork/FinalProject/src/finalproject/MenuContainer.java
+++ schoolWork/FinalProject/src/finalproject/MenuContainer.java
@@ -20,10 +20,13 @@ public class MenuContainer {
     Utilities.ValidateInput input = toolbox.new ValidateInput(); // This constructor is used for validating input
     Scanner scan = new Scanner(System.in);
     
+    // This class will hold all methods correlating to the main menu
     public class MainMenu{
         
+        RecipeMenu recipe = new RecipeMenu(); // Constructor to go to recipe menu
+        
         public void displayScreen(){
-            String menuDir = getPath.menuFolder() + "/mainScreen.txt";  // Uses our toolbox constructor to pull the directory for menu folder.
+            String menuDir = getPath.menuFolder() + "/mainScreen.txt";   // Uses our toolbox constructor to pull the directory for menu folder.
             readText.displayText(menuDir);                               // Displays our text file from the directory we pulled.
             
             int maxRange = 3;                                            // Highest number option user can choose.
@@ -36,12 +39,52 @@ public class MenuContainer {
         }
         public void jumpTree(int userChoice){
             switch(userChoice){
-                case 1:  // To recipe menu
+                case 1:  recipe.displayScreen();
                     break;
                 case 2:  // To algorithsm
                     break;
                 default: System.out.println("There was an error in \"MainMenu.jumptree\"");
             }
         }
+    }
+    // This class will hold all methods correlating to the recipe menu
+    public class RecipeMenu{
+        public void displayScreen(){
+            String menuDir = getPath.menuFolder() + "/recipeScreen.txt"; // Uses our toolbox constructor to pull the directory for menu folder.
+            readText.displayText(menuDir);                               // Displays our text file from the directory we pulled.
+            
+            int maxRange = 7;                                            // Highest number option user can choose.
+            String userInput = scan.nextLine();
+            
+            while(input.validateInteger(userInput, maxRange) != true){   // Checks for a "true" value from method validate to see if the input is a number.
+                userInput = scan.nextLine();
+            }
+            jumpTree(Integer.parseInt(userInput));
+        }
+        public void jumpTree(int userChoice){
+            String recipeDir = getPath.recipeFolder();
+            switch(userChoice){
+                case 1: readText.displayText(recipeDir + "/bakedChickenPotatoFinal.txt");
+                    break;
+                case 2: readText.displayText(recipeDir + "/beefstewFinal.txt");
+                    break;
+                case 3: readText.displayText(recipeDir + "/chickenCacciatore.txt");
+                    break;
+                case 4: readText.displayText(recipeDir + "/chickenStewFinal.txt");
+                    break;
+                case 5: readText.displayText(recipeDir + "/sweetSourChickenFinal.txt");
+                    break;
+                case 6: readText.displayText(recipeDir + "/sweetSourChicken.txt");
+                    break;
+                case 7: // Return to main menu
+                    break;
+                default: System.out.println("An error has occured in \"MenuContainer.RecipeMenu.jumpTree\"");
+                    break;
+            }
+        }
+    }
+    // This class will hold all methods correlating to the algorithm menu
+    public class AlgorithmMenu{
+        
     }
 }
